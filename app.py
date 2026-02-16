b2d83562aa8762b5a3a0a4b045a42b224b79ec3c
Prachi142 <prachijaglan143@gmail.com>
2026-02-16 23:18:44 +0530

    Configure app to run on specified port
    Update Flask app to use environment variable for port.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import os
 
 app = Flask(__name__)
 
@@ -7,8 +8,8 @@ app = Flask(__name__)
 def home():
     # fake risk value (later tum model se replace karna)
     risk = random.randint(10, 95)
-
     return render_template("index.html", risk=risk)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render ka port ya local 5000
+    app.run(host="0.0.0.0", port=port)
